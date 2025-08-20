f8e8840c4a42a010c36d2986a968c0b33ca0985c
divyashahhh <e1355573@u.nus.edu>
2025-08-20 18:28:24 +0800

    Level 2

--- src/main/java/Paneer.java
+++ src/main/java/Paneer.java
@@ -1,33 +1,60 @@
 import java.util.Scanner;
+
 public class Paneer {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         String logo =
-                " ____                              \n"
-                        + "|  _ \\   __ _  _ __   ___  ___ _ __ \n"
-                        + "| |_) | / _` || '_ \\ / _ \\/ _ \\ '__|\n"
-                        + "|  __/ | (_| || | | |  __/  __/ |   \n"
-                        + "|_|     \\__,_||_| |_|\\___|\\___|_|   \n";
+                """
+                         ____                             \s
+                        |  _ \\   __ _  _ __   ___  ___ _ __\s
+                        | |_) | / _` || '_ \\ / _ \\/ _ \\ '__|
+                        |  __/ | (_| || | | |  __/  __/ |  \s
+                        |_|     \\__,_||_| |_|\\___|\\___|_|  \s
+                        """;
         System.out.println("Hello from\n" + logo);
-        String line = "____________________________________________________________";
-        System.out.println(line);
-        System.out.println("Hello! I'm Paneer");
-        System.out.println("What can I do for you today?");
+
+        final String LINE = "____________________________________________________________";
+
+        String[] list = new String[100];
+        int size = 0;
+
+        System.out.println(LINE);
+        System.out.println(" Hello! I'm Paneer");
+        System.out.println(" What can I do for you?");
+        System.out.println(LINE);
 
         while (true) {
-            String input = s.nextLine();
-            if (input.equals("bye") || input.equals("Bye")) {
-                System.out.println(line);
-                System.out.println("Byeeeeee! Paneer shall serve you another day!");
-                System.out.println(line);
+            String input = sc.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(LINE);
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(LINE);
                 break;
             }
-            System.out.println(line);
-            System.out.println(input);
-            System.out.println(line);
+
+            if (input.equalsIgnoreCase("list")) {
+                System.out.println(LINE);
+                for (int i = 0; i < size; i++) {
+                    System.out.println(" " + (i + 1) + ". " + list[i]);
+                }
+                System.out.println(LINE);
+                continue;
+            }
+            if (size < list.length) {
+                list[size] = input;
+                size++;
+                System.out.println(LINE);
+                System.out.println(" added: " + input);
+                System.out.println(LINE);
+            } else {
+                System.out.println(LINE);
+                System.out.println(" Sorry, I can only store up to 100 list.");
+                System.out.println(LINE);
+            }
         }
 
+        sc.close();
     }
-
-
 }
+
