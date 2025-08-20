b06a85104a574a69aeca30714b68278edf2d6628
divyashahhh <e1355573@u.nus.edu>
2025-08-20 17:39:07 +0800

    Level 0

---
+++ src/main/java/Paneer.java
@@ -0,0 +1,23 @@
+
+public class Paneer {
+    public static void main(String[] args) {
+
+        String logo =
+                " ____                              \n"
+                        + "|  _ \\   __ _  _ __   ___  ___ _ __ \n"
+                        + "| |_) | / _` || '_ \\ / _ \\/ _ \\ '__|\n"
+                        + "|  __/ | (_| || | | |  __/  __/ |   \n"
+                        + "|_|     \\__,_||_| |_|\\___|\\___|_|   \n";
+        System.out.println("Hello from\n" + logo);
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm Paneer");
+        System.out.println("What can I do for you today?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon");
+        System.out.println(line);
+
+    }
+
+
+}
