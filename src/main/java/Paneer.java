8b31a420f7071d6365bc370f8f779ca0919ebada
divyashahhh <e1355573@u.nus.edu>
2025-08-20 18:52:46 +0800

    Level 3

--- src/main/java/Paneer.java
+++ src/main/java/Paneer.java
@@ -15,41 +15,69 @@ public class Paneer {
 
         final String LINE = "____________________________________________________________";
 
-        String[] list = new String[100];
+        Task[] tasks = new Task[100];
         int size = 0;
 
         System.out.println(LINE);
         System.out.println(" Hello! I'm Paneer");
-        System.out.println(" What can I do for you?");
+        System.out.println(" What can I do for you today?");
         System.out.println(LINE);
 
         while (true) {
             String input = sc.nextLine();
 
+            // exit
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println(LINE);
-                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(" Byeeee! Paneer shall serve you another time!");
                 System.out.println(LINE);
                 break;
             }
 
+            // list
             if (input.equalsIgnoreCase("list")) {
                 System.out.println(LINE);
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < size; i++) {
-                    System.out.println(" " + (i + 1) + ". " + list[i]);
+                    System.out.println(" " + (i + 1) + "." + tasks[i]);
                 }
                 System.out.println(LINE);
                 continue;
             }
-            if (size < list.length) {
-                list[size] = input;
+
+            // mark
+            if (input.startsWith("mark ")) {
+                char c = input.charAt(input.length() - 1); // last char
+                int index = (c - '0') - 1;                 // convert char to number
+                if (index >= 0 && index < size) {
+                    tasks[index].markDone();
+                    System.out.println(LINE);
+                    System.out.println(" Nice! I've marked this task as done:");
+                    System.out.println("   " + tasks[index]);
+                    System.out.println(LINE);
+                }
+                continue;
+            }
+
+            // unmark
+            if (input.startsWith("unmark ")) {
+                char c = input.charAt(input.length() - 1); //last char
+                int index = (c - '0') - 1;
+                if (index >= 0 && index < size) {
+                    tasks[index].markUndone();
+                    System.out.println(LINE);
+                    System.out.println(" OK, I've marked this task as not done yet:");
+                    System.out.println("   " + tasks[index]);
+                    System.out.println(LINE);
+                }
+                continue;
+            }
+            // add task
+            if (size < tasks.length) {
+                tasks[size] = new Task(input);
                 size++;
                 System.out.println(LINE);
                 System.out.println(" added: " + input);
-                System.out.println(LINE);
-            } else {
-                System.out.println(LINE);
-                System.out.println(" Sorry, I can only store up to 100 list.");
                 System.out.println(LINE);
             }
         }
